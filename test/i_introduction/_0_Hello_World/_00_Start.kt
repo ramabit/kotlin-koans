994b0ce62851b7a29daf2ae9da770f6e8b5db652
ramabit <f.e.ramirez94@gmail.com>
2016-01-18 15:23:14 -0300

    first 7 exercises done

--- test/i_introduction/_0_Hello_World/_00_Start.kt
+++ test/i_introduction/_0_Hello_World/_00_Start.kt
@@ -1,7 +1,8 @@
-package i_introduction._0_Hello_World.Hello
+package i_introduction._0_Hello_World
 
 import junit.framework.Assert
 import org.junit.Test
+import i_introduction._0_Hello_World.Hello.task0
 
 class _00_Start {
     @Test fun testOk() {
