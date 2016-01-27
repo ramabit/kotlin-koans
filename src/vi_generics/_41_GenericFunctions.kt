733cb560dc8444d01421c9849b47f28dabe5d9b9
ramabit <f.e.ramirez94@gmail.com>
2016-01-27 14:31:04 -0300

    complete generic function exercise

--- src/vi_generics/_41_GenericFunctions.kt
+++ src/vi_generics/_41_GenericFunctions.kt
@@ -20,12 +20,21 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun  <T, R: MutableCollection<T>> Collection<T>.partitionTo(thing: R, otherThing: R, condition: (T) -> Boolean): Pair<R, R> {
+    for(element in this){
+        if(condition(element)){
+            thing.add(element)
+        }else{
+            otherThing.add(element)
+        }
+    }
+    return Pair(thing, otherThing)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
